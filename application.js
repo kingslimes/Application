fbd40831611f6c2f65687a079a41694af5242daa
kingslimes <animeplayermaster@gmail.com>
2022-12-06 14:30:41 +0700

    add dashboard page

--- application.js
+++ application.js
@@ -21,10 +21,10 @@ const chapter = new slimeDB('chapter').createTable({
   images: slimeDB.DataType.VARCHAR
 })
 
-client.post('/api/v1/files/create', async ( req, res ) => {
-  if ( !req.session.login ) return res.status( 403 );
+client.post('/api/v1/files/create', async (req, res) => {
+  if (!req.session.login) return res.status(403);
   const pathName = './stream/' + short.generate() + '.stream';
-  fs.writeFileSync(pathName, req.body.image, { encoding:'base64' });
+  fs.writeFileSync(pathName, req.body.image, { encoding: 'base64' });
   try {
     const auth = new google.auth.GoogleAuth({
       keyFile: './google.json',
@@ -35,7 +35,7 @@ client.post('/api/v1/files/create', async ( req, res ) => {
       auth
     })
     const fileMetaData = {
-      'name': `${ new Date().getTime() }`,
+      'name': `${new Date().getTime()}`,
       'parents': [GOOGLE_API_FOLDER_ID]
     }
     const media = {
@@ -51,16 +51,16 @@ client.post('/api/v1/files/create', async ( req, res ) => {
       id: response.data.id,
       name: req.body.name
     })
-  } catch(err) {
+  } catch (err) {
     res.status(403).json({
-        message: err
+      message: err
     })
   }
   fs.unlinkSync(pathName);
 });
 
-client.post('/api/v1/files/delete', async ( req, res ) => {
-  if ( !req.session.login ) return res.status( 403 );
+client.post('/api/v1/files/delete', async (req, res) => {
+  if (!req.session.login) return res.status(403);
   try {
     const auth = new google.auth.GoogleAuth({
       keyFile: './google.json',
@@ -76,7 +76,7 @@ client.post('/api/v1/files/delete', async ( req, res ) => {
     res.json({
       id: req.body.id,
     })
-  } catch(err) {
+  } catch (err) {
     res.status(403).json({
       message: err
     })
@@ -84,7 +84,7 @@ client.post('/api/v1/files/delete', async ( req, res ) => {
 });
 
 // set home page
-client.get('/', ( req, res ) => {
+client.get('/', (req, res) => {
   req.flash('login', req.session.login);
   req.flash('username', req.session.username);
   req.flash('homePath', `/${req.session.username}/`);
@@ -92,18 +92,22 @@ client.get('/', ( req, res ) => {
   res.render('index');
 })
 
+client.get('/:username/dashboard/', (req, res) => {
+  res.render('dashboard');
+})
+
 // set upload page
-client.get('/:username/dashboard/upload/:id', ( req, res ) => {
-  if ( !req.session.login || req.session.username != req.params.username ) return res.redirect( `/${req.params.username}/` );
+client.get('/:username/dashboard/upload/:id', (req, res) => {
+  if (!req.session.login || req.session.username != req.params.username) return res.redirect(`/${req.params.username}/`);
   res.render('upload');
 })
-client.post('/:username/dashboard/upload/:id', ( req, res ) => {
-  if ( !req.session.login || req.session.username != req.params.username ) return res.status( 403 );
+client.post('/:username/dashboard/upload/:id', (req, res) => {
+  if (!req.session.login || req.session.username != req.params.username) return res.status(403);
   let data = {
     manga_id: req.params.id,
     images: req.body.images
   }
-  slimeDB.insert( chapter, data );
+  slimeDB.insert(chapter, data);
   res.json({
     message: "success",
     data: req.body
@@ -111,42 +115,42 @@ client.post('/:username/dashboard/upload/:id', ( req, res ) => {
 })
 
 // set register page
-client.get('/register', ( req, res ) => {
-  if ( req.session.login ) return res.redirect( req.session.dashboard );
+client.get('/register', (req, res) => {
+  if (req.session.login) return res.redirect(req.session.dashboard);
   req.session.status = 'register';
   res.render('register');
 })
-client.post('/register', ( req, res ) => {
-  if ( req.session.login ) return res.redirect( req.session.dashboard );
-  if ( !req.body.email || !req.body.username || !req.body.password || req.session.status !== 'register' ) return res.redirect('./');
+client.post('/register', (req, res) => {
+  if (req.session.login) return res.redirect(req.session.dashboard);
+  if (!req.body.email || !req.body.username || !req.body.password || req.session.status !== 'register') return res.redirect('./');
   let email = req.body.email;
   let username = req.body.username.toLowerCase();
   let password = req.body.password;
   let isExists = false;
-  slimeDB.query( member, table => {
-    let existsUsername = table.find( i => i.username == username );
-    let existsEmail = table.find( i => i.email == email );
-    if ( existsUsername ) {
+  slimeDB.query(member, table => {
+    let existsUsername = table.find(i => i.username == username);
+    let existsEmail = table.find(i => i.email == email);
+    if (existsUsername) {
       req.flash('error', 'username');
       isExists = true
     }
-    if ( existsEmail ) {
+    if (existsEmail) {
       req.flash('error', 'email');
       isExists = true
     }
-    if ( !isExists ) {
-      slimeDB.insert( member, {
+    if (!isExists) {
+      slimeDB.insert(member, {
         root: password,
         email, email,
         username: username,
-        password: md5( password ),
+        password: md5(password),
         timestamp: new Date().getTime()
       });
       req.session.login = true;
       req.session.username = username;
       req.session.dashboard = `/${username}/dashboard/`;
       req.session.timestamp = new Date().getTime();
-      res.redirect( req.session.dashboard );
+      res.redirect(req.session.dashboard);
     } else {
       req.flash('email', email);
       req.flash('username', username);
@@ -157,33 +161,33 @@ client.post('/register', ( req, res ) => {
 })
 
 // set login page
-client.get('/login', ( req, res ) => {
-  if ( req.session.login ) return res.redirect( req.session.dashboard );
+client.get('/login', (req, res) => {
+  if (req.session.login) return res.redirect(req.session.dashboard);
   req.session.status = 'login';
   res.render('login');
 })
-client.post('/login', ( req, res ) => {
-  if ( req.session.login ) return res.redirect( req.session.dashboard );
-  if ( !req.body.username || !req.body.password || req.session.status !== 'login' ) return res.redirect('./');
+client.post('/login', (req, res) => {
+  if (req.session.login) return res.redirect(req.session.dashboard);
+  if (!req.body.username || !req.body.password || req.session.status !== 'login') return res.redirect('./');
   let username = req.body.username.toLowerCase();
   let password = req.body.password;
   let loginNotSuccess = false;
-  slimeDB.query( member, table => {
-    let isResult = table.find( i => i.username == username );
-    if ( !isResult ) {
+  slimeDB.query(member, table => {
+    let isResult = table.find(i => i.username == username);
+    if (!isResult) {
       req.flash('error', 'username');
       loginNotSuccess = true
     }
-    if ( isResult && isResult.password !== md5( password ) ) {
+    if (isResult && isResult.password !== md5(password)) {
       req.flash('error', 'password');
       loginNotSuccess = true
     }
-    if ( !loginNotSuccess ) {
+    if (!loginNotSuccess) {
       req.session.login = true;
       req.session.username = username;
       req.session.dashboard = `/${username}/dashboard/`;
       req.session.timestamp = new Date().getTime();
-      res.redirect( req.session.dashboard );
+      res.redirect(req.session.dashboard);
     } else {
       req.flash('username', username);
       req.flash('password', password);
@@ -193,9 +197,9 @@ client.post('/login', ( req, res ) => {
 });
 
 // set page not be found
-client.get('*', ( req, res ) => {
+client.get('*', (req, res) => {
   res.status(404).render('404')
 });
-client.post('*', ( req, res ) => {
+client.post('*', (req, res) => {
   res.status(404).render('404')
 });
